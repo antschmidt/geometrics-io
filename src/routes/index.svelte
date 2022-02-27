b0977494b7d7fc92d28cbd92d4e9b1d2d99ef06e
Anthony Schmidt <gnomadic@gmail.com>
2022-02-27 17:28:11 -0600

    new geometrics

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Tony from '../images/tony.svelte'
+</script>
+<main>
+<h1>Coming Soon</h1>
+
+<h2 class='head'>
+<Tony />
+</h2>
+</main>
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-size: 3rem;
+        margin-top: 10px;
+        text-align: center;
+        width: 100vw;
+    }
+    .head {
+        height: 20vh;
+        width: 10vw;
+    }
+</style>
