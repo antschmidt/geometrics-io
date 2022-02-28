cd65633ce23e10701d78fd556855ddbf44f3c3a1
Anthony Schmidt <gnomadic@gmail.com>
2022-02-27 19:11:26 -0600

    slight sizing adjustment

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,13 +13,27 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-        font-size: 3rem;
+        font-size: 1rem;
         margin-top: 10px;
         text-align: center;
         width: 100vw;
     }
+
+    @media (min-width: 1280px) {
+        main {
+            font-size: 3rem;
+        }
+    }
+
     .head {
-        height: 20vh;
-        width: 10vw;
+        min-height: 30vh;
+        max-height: 75vh;
+        min-width: 50vw;
+        max-width: 80vw;
+    }
+    @media (min-width: 1280px) {
+        .head {
+            min-width: 10vw;
+        }
     }
 </style>
